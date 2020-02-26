42c9630d066d4d486dc9a15ac1d2e335c7521c7a
Joe Barnes <joebarnes333@gmail.com>
2020-02-25 22:56:49 -0800

    add login service

--- DatingApp-SPA/src/app/app.module.ts
+++ DatingApp-SPA/src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from "@angular/core";
 import { HttpClientModule } from "@angular/common/http";
 import { AppComponent } from "./app.component";
 import { ValueComponent } from "./value/value.component";
+import { NavComponent } from "./nav/nav.component";
+import { FormsModule } from "@angular/forms";
 
 @NgModule({
-  declarations: [AppComponent, ValueComponent],
-  imports: [BrowserModule, HttpClientModule],
+  declarations: [AppComponent, ValueComponent, NavComponent],
+  imports: [BrowserModule, HttpClientModule, FormsModule],
   providers: [],
   bootstrap: [AppComponent]
 })
