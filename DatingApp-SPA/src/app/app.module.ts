7c88a9f1c881e8d0d01a4bea44fc43c6ddf16612
Joe Barnes <joebarnes333@gmail.com>
2020-02-26 22:02:48 -0800

    adjusted home page

--- DatingApp-SPA/src/app/app.module.ts
+++ DatingApp-SPA/src/app/app.module.ts
@@ -6,11 +6,27 @@ import { AppComponent } from "./app.component";
 import { ValueComponent } from "./value/value.component";
 import { NavComponent } from "./nav/nav.component";
 import { FormsModule } from "@angular/forms";
+import { HomeComponent } from './home/home.component';
+import { RegisterComponent } from './register/register.component';
 
 @NgModule({
-  declarations: [AppComponent, ValueComponent, NavComponent],
-  imports: [BrowserModule, HttpClientModule, FormsModule],
-  providers: [AuthService],
-  bootstrap: [AppComponent]
+   declarations: [
+      AppComponent,
+      ValueComponent,
+      NavComponent,
+      HomeComponent,
+      RegisterComponent
+   ],
+   imports: [
+      BrowserModule,
+      HttpClientModule,
+      FormsModule
+   ],
+   providers: [
+      AuthService
+   ],
+   bootstrap: [
+      AppComponent
+   ]
 })
 export class AppModule {}
